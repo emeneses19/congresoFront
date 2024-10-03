6f7e060eaa12e7a355aacea23b3fc0673baa9fcc
unknown <menesetaipe19@gmail.com>
2024-10-02 22:17:14 -0500

    avance de evento ya esta la creacion del objeto

--- src/app/components/auxiliares/evento/evento.component.ts
+++ src/app/components/auxiliares/evento/evento.component.ts
@@ -1,16 +1,56 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-// import { AgregarEventoComponent } from './agregar-evento/agregar-evento.component';
-
+import { EventoModel } from '../../../models/evento.model';
+import { CongresoModel } from '../../../models/congreso.model';
+import { CongresoService } from '../../../services/congreso.service';
+import { NgForm } from '@angular/forms';
+import { EventoService } from '../../../services/evento.service';
 @Component({
   selector: 'app-evento',
   templateUrl: './evento.component.html',
   styleUrl: './evento.component.css'
 })
 export class EventoComponent {
-  constructor(public dialog: MatDialog) {}
-  // openAgregarEvento(){
-  //   this.dialog.open(AgregarEventoComponent);
-  // }
+  textoError: string ='';
+  error: boolean = false;
+  cargando: boolean = false;
+  evento: EventoModel = new EventoModel(new CongresoModel());
+  listaDeEvento: EventoModel[]= [];
+  eventoActualizar: EventoModel = new EventoModel(new CongresoModel());
+  listaDeCongresos: CongresoModel[] = [];
+  
+  constructor(private _congresoServices: CongresoService,
+              private _eventoServices: EventoService
+  ) {
+    this.obtenerCongresos();
+  }
+  guardarEvento(form: NgForm){
+    if(form.invalid){
+      this.error = true;
+      this.textoError = 'Formulario incorrecto. Por favor, revíselo.';
+      return;
+    }else{
+      this.error = false;
+      this._eventoServices.agregarEvento(this.evento);
+      console.log(this.listaDeEvento);
+    }
+  }
+
+  selectedCongreso(congreso: CongresoModel){
+    this.evento = new EventoModel(congreso);
+  }
+
+  obtenerCongresos(){
+  this._congresoServices.obtenerCongreso().subscribe(
+    (data:CongresoModel[]) =>{
+      this.listaDeCongresos = data;
+    },
+    (error)=>{
+      console.log(error);
+    }
+  )
+  }
+
+
 
 }
